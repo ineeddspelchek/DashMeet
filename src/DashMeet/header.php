7ce4a25eba486826b1a0ba634113708b1d1b7327
ineeddspelchek <hanewton35@gmail.com>
2024-04-02 19:46:47 -0400

    Update header.php

--- src/DashMeet/header.php
+++ src/DashMeet/header.php
@@ -1,10 +1,10 @@
 <header>
     <nav class="navbar navbar-light">
-        <a class="navbar-brand" href="hostMain.html">DashMeet</a>
+        <a class="navbar-brand" href="?command=hostMain">DashMeet</a>
         <form class="form-inline">
             <a class="account" href="?command=account">
                 <?php echo $email;?>
-                <img src="/images/account.png" alt="account icon">
+                <img src="images/account.png" alt="account icon">
             </a>
         </form>
     </nav>
