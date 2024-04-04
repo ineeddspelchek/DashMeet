e7a42c52ba476a19a2829932401abfe9b673e59f
ineeddspelchek <hanewton35@gmail.com>
2024-04-03 21:59:18 -0400

    finished JSON output

--- src/DashMeet/import.php
+++ src/DashMeet/import.php
@@ -1,30 +1,80 @@
 <?php
     // Sources:
     // https://stackoverflow.com/questions/4064444/returning-json-from-a-php-script
+    // https://icalendar.org/
 
     $importStr = file_get_contents($_FILES["import"]["tmp_name"], true);
     $explodedStr = explode("BEGIN:VEVENT", $importStr);
 
     $outJSON = open($explodedStr);
-    
     for ($i=1; $i < count($explodedStr); $i++) { 
         $outJSON .= addEvent($explodedStr[$i]);
     }
-
-    $outJSON .= "}";
+    $outJSON = rtrim($outJSON, ","); #remove comma added by last event
+    $outJSON .= close();
 
 
     header('Content-Type: application/json; charset=utf-8');
     echo $outJSON;
 
+
+
+
     function open($explodedStr) {
         $element0 = $explodedStr[0];
         $out = "{";
-        $out .= "\"name\": \" .  . \"";
+        preg_match("/\nX-WR-CALNAME:.*\n/", $element0, $nameReg);
+        preg_match("/\nX-WR-TIMEZONE:.*\n/", $element0, $timezoneReg);
+        $name = $nameReg[0];
+        $timezone = $timezoneReg[0];
+
+        $out .= "\"name\": \"" . addcslashes(trim(explode(":", $name)[1]), "\\") . "\",";
+        $out .= "\"timezone\": \"" . addcslashes(trim(explode(":", $timezone)[1]), "\\") . "\",";
+        $out .= "\"events\": " . "[";
         return $out;
     }
 
     function addEvent($inp) {
+        $out = "{";
 
+        if(preg_match("/\nSUMMARY:.*\n/", $inp, $nameReg)) {
+            $name = $nameReg[0];
+            $out .= "\n\"name\": \"" . addcslashes(trim(explode(":", $name)[1]), "\\") . "\",";
+        }
+
+        if(preg_match("/\nDTSTART:.*\n/", $inp, $startReg)) {
+            $start = $startReg[0];
+            $out .= "\"start\": \"" . trim(explode(":", $start)[1]) . "\",";
+        }
+        elseif(preg_match("/\nDTSTART;.*\n/", $inp, $startReg)) {
+            $start = $startReg[0];
+            $out .= "\"start\": \"" . trim(explode(":", $start)[1]) . "\",";
+        }
+
+        if(preg_match("/\nDTEND:.*\n/", $inp, $endReg)) {
+            $end = $startReg[0];
+            $out .= "\"end\": \"" . trim(explode(":", $end)[1]) . "\",";
+        }
+        elseif(preg_match("/\nDTEND;.*\n/", $inp, $endReg)) {
+            $end = $startReg[0];
+            $out .= "\"end\": \"" . trim(explode(":", $end)[1]) . "\",";
+        }
+
+        if(preg_match("/\nRRULE:FREQ=.*\n/", $inp, $repeatsReg)) {
+            $repeats = $repeatsReg[0];
+            $out .= "\"repeats\": \"" . trim(explode("=", explode(";", $repeats)[0])[1]) . "\",";
+            if(trim(explode("=", explode(";", $repeats)[0])[1]) === "WEEKLY") {
+                $out .= "\"repeatsOn\": \"" . trim(explode("=", explode(";", $repeats)[1])[1]) . "\",";
+            }
+        }
+
+        $out = rtrim($out, ",");
+       
+        $out .= "},";
+        return $out;
+    }
+
+    function close() {
+        return "]}";
     }
 ?>
